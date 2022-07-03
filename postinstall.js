97bc390ef19b4549f7004ee2725e1aa3a4353fd4
TA2k <tombox2020@gmail.com>
2022-07-03 19:30:02 +0200

    fix postinstall

--- postinstall.js
+++ postinstall.js
@@ -1,5 +1,5 @@
 const { execSync } = require("child_process");
-console.log("Start Post install");
+console.log("Start Post Python install");
 let result = "";
 try {
     result = execSync("python3 -V");
@@ -23,8 +23,14 @@ if (!result.includes("Python 3")) {
 try {
     result = execSync("pip3 -V");
 } catch (error) {
-    console.log("pip not found. Please install pip3. Via 'sudo apt install python3-pip'");
-    process.exit(1);
+    console.log("pip not found. Try to install local.'");
+    try {
+        result = execSync("wget https://bootstrap.pypa.io/get-pip.py");
+        result = execSync("python3 get-pip.py --user");
+    } catch (error) {
+        console.log("Local pip installation failed. Please install via 'sudo apt install python3-pip'");
+        process.exit(1);
+    }
 }
 result = result.toString("utf8");
 console.log(result);
